63715ab7a01e347f22c163a86900ca54b5ec96e2
renato <ren.rib.silva@gmail.com>
2026-01-14 17:05:50 -0300

    arquivos modificados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,25 @@ import { ThemeProvider } from '../components/tsx/theme_provider';
 import './globals.css';
 import { Ubuntu } from "next/font/google"
 import { SidebarProvider } from '../context/sidebar_context';
+import { Metadata } from 'next';
 
 const roboto = Ubuntu({
   subsets: ["latin"],
   weight: ["300", "400"],
 });
+
+export const metadata: Metadata = {
+  title: 'Microenem',
+  description: 'Análise estatística dos microdados do ENEM',
+  twitter: {
+    card: 'summary_large_image', // Tipo do card: summary ou summary_large_image
+    title: 'Microenem',
+    description: 'Análise estatística dos microdados do ENEM',
+    siteId: '1467726470533754880',
+    creator: '@renribsilva',
+    images: ['https://seusite.com/og-image.png'], // URL absoluta da imagem
+  },
+}
 
 
 export default function RootLayout({

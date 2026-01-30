046d5d4e3d54eb1eed257809f6ebb1bdab69bb7b
renato <ren.rib.silva@gmail.com>
2026-01-30 08:55:20 -0300

    arquivos modificados

--- src/components/tsx/sidebar.tsx
+++ src/components/tsx/sidebar.tsx
@@ -25,7 +25,7 @@ const icons = {
   arrow_up: ArrowUp
 }
 
-const anosPermitidos = ["2019", "2020", "2021", "2022", "2023"];
+const anosPermitidos = ["2020", "2021", "2022", "2023"];
 
 // Função auxiliar para gerar sub-itens com base no ano
 const generateSubItems = (year: string) => [

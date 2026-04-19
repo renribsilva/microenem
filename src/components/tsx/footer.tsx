d09167228e40074d5a7ed86fd36428dd3eceb273
renato <ren.rib.silva@gmail.com>
2026-04-19 16:08:37 -0300

    arquivos modificados

--- src/components/tsx/footer.tsx
+++ src/components/tsx/footer.tsx
@@ -6,7 +6,7 @@ const currentYear = new Date().getFullYear();
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <div>MIT © {currentYear}</div>
+      <div>GPLv3 © {currentYear}</div>
       <div>No bullshit, just data.</div>
       <div className={styles.links_container}>
         <span>

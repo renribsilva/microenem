6177abfd24e547421d1bbcdc03e1192dba4ce95e
Renato <ren.rib.silva@gmail.com>
2026-05-27 10:19:47 -0300

    arquivos modificados

---
+++ src/app/(home)/[...not-found]/page.tsx
@@ -0,0 +1,13 @@
+function Custum404() {
+  return (
+    <section>
+      <h1>Eita!!!</h1>
+      <p>
+        A página que você procura não foi encontrada. Para encontrar a página
+        que deseja, navegue no menu.
+      </p>
+    </section>
+  );
+}
+
+export default Custum404;

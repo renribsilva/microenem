a22062c0817d33e7705bd1854bd44a6754c28e15
Renato <ren.rib.silva@gmail.com>
2026-07-08 21:53:57 -0300

    arquivos modificados

--- src/components/tsx/pdf_thumbnail.tsx
+++ src/components/tsx/pdf_thumbnail.tsx
@@ -96,7 +96,7 @@ export default function PdfThumbnail({
                 <Page
                   pageNumber={pageNumber}
                   scale={scale}
-                  onLoadSuccess={handlePageLoad}
+                  onRenderSuccess={handlePageLoad}
                   renderTextLayer={false}
                   renderAnnotationLayer={false}
                 />

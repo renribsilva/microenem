9b4125310345e18498fdfaea041e4090acd409ca
Renato <ren.rib.silva@gmail.com>
2026-07-14 09:51:02 -0300

    arquivos modificados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,42 +1,61 @@
-import ThemeProvider from "../components/tsx/theme_provider";
-import "./globals.css";
+import "../globals.css";
 import { Ubuntu } from "next/font/google";
-import { SidebarProvider } from "../context/sidebar_context";
 import { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
+import { SidebarProvider } from "../context/sidebar_context";
 
 const roboto = Ubuntu({
   subsets: ["latin"],
   weight: ["300", "400"],
 });
 
-export const metadata: Metadata = {
-  metadataBase: new URL("https://microenem.vercel.app"),
-  title: "ENEMmicro",
-  description: "Vizualização gráfica dos microdados do ENEM",
-  twitter: {
-    card: "summary_large_image",
-    title: "ENEMmicro",
-    description: "Vizualização gráfica dos microdados do ENEM",
-    siteId: "1467726470533754880",
-    creator: "@renribsilva",
-  },
-  openGraph: {
-    title: "ENEMmicro",
-    siteName: "ENEMmicro",
-    description: "Vizualização gráfica dos microdados do ENEM",
-    type: "website",
-    url: "https://microenem.vercel.app",
-  },
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<Record<string, string | string[]>>;
+}): Promise<Metadata> {
+  const resolvedParams = await params;
 
-function RootLayout({ children }: { children: React.ReactNode }) {
+  const path = Object.values(resolvedParams).flat().join("/");
+
+  const url = `https://microenem.vercel.app/${path}`;
+
+  return {
+    metadataBase: new URL("https://microenem.vercel.app"),
+
+    title: "ENEMmicro",
+    description: "Vizualização gráfica dos microdados do ENEM",
+
+    twitter: {
+      card: "summary_large_image",
+      title: "ENEMmicro",
+      description: "Vizualização gráfica dos microdados do ENEM",
+      siteId: "1467726470533754880",
+      creator: "@renribsilva",
+    },
+
+    openGraph: {
+      title: "ENEMmicro",
+      siteName: "ENEMmicro",
+      description: "Vizualização gráfica dos microdados do ENEM",
+      type: "website",
+      url,
+    },
+  };
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
       <body className={roboto.className}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
-          enableSystem={true}
+          enableSystem
           disableTransitionOnChange
         >
           <SidebarProvider>{children}</SidebarProvider>
@@ -45,5 +64,3 @@ function RootLayout({ children }: { children: React.ReactNode }) {
     </html>
   );
 }
-
-export default RootLayout;

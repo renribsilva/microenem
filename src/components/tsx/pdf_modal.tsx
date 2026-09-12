d280c83c2605a9310e4e3f9a94fc73c58d2c9873
Renato <ren.rib.silva@gmail.com>
2026-09-12 19:23:10 -0300

    arquivos modificados

--- src/components/tsx/pdf_modal.tsx
+++ src/components/tsx/pdf_modal.tsx
@@ -317,7 +317,6 @@ export default function PdfModal({
               width: "max-content",
               minWidth: "max-content",
               padding: "14px",
-              paddingRight: "0px",
               marginRight: "0px",
               boxSizing: "border-box",
             }}
